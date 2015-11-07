6babce6a52276fbef742ec50d46cb8f832ab1c48
Florent <Wr0ngName@users.noreply.github.com>
2015-11-07 14:46:35 +0100

    handle unexpected counter access

--- admin/counter.php
+++ admin/counter.php
@@ -1,4 +1,6 @@
 <?php
+  if(!isset($cur_Route)) header('Location:../index.php?admin');
+  
   $db = new PDO('sqlite:./core/data/counter.db.sqlite');
 
   $result = $db->query('SELECT * FROM counter WHERE id=1');
